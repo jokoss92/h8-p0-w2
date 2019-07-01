489dead63a7db73ed8a80ecc7eec8005a7a115c8
Joko Sarjono Slameto <johno.smakaduta@gmail.com>
2019-07-01 17:49:19 +0700

    Exercise 4

---
+++ exercise4.js
@@ -0,0 +1,54 @@
+var hari = 28; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = 12; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tahun = 1992; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+switch(bulan){
+    case 1:
+        bulan = "Januari";
+        break;
+    case 2:
+        bulan = "Februari";
+        break;
+    case 3:
+        bulan = "Maret";
+        break;
+    case 4:
+        bulan = "April";
+        break;
+    case 5:
+        bulan = "Mei";
+        break;
+    case 6:
+        bulan = "Juni";
+        break;
+    case 7:
+        bulan = "Juli";
+        break;
+    case 8:
+        bulan = "Agustus";
+        break;
+    case 9:
+        bulan = "September";
+        break;
+    case 10:
+        bulan = "Oktober";
+        break;
+    case 11:
+        bulan = "November";
+        break;
+    case 12:
+        bulan = "Desember";
+        break;
+    default:
+        break;
+}
+
+if(hari <= 1 || hari >= 31 || bulan <= 1 && bulan >= 12|| tahun <= 1900 || tahun >= 2200  ){
+    console.log("Tanggal tidak sesuai")
+} else {
+    tanggal = hari + " " + bulan + " " + tahun;
+    console.log(tanggal);
+}
+
+
+
+
