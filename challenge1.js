dddf5c09ff60382445bb1dded9aaa87d2b18d029
Joko Sarjono Slameto <johno.smakaduta@gmail.com>
2019-07-01 15:47:05 +0700

    Revision 1

--- challenge1.js
+++ challenge1.js
@@ -1,8 +1,8 @@
 //Soal 1 Newton Second Law
-// var m = 600;
-// var a = 2;
-// var F = m*a;
-// console.log(F);
+var m = 600;
+var a = 2;
+var F = m*a;
+console.log(F);
 
 /*
 a. Algorithm
@@ -25,15 +25,15 @@ END
 */
 
 //Soal 2 Tahun Kabisat
-// var year = 2018;
-// if(year % 4 == 0 && year % 100 !== 0 || year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
-//     console.log("Tahun Kabisat");  
-// } else {
-//     console.log("Bukan Tahun Kabisat");
-// }
+var year = 2018;
+if(year % 4 == 0 && year % 100 !== 0 || year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+    console.log("Tahun Kabisat");  
+} else {
+    console.log("Bukan Tahun Kabisat");
+}
 
 /*
-a. Algoritma
+a. Algorithm
 1. Get value of year
 2. Calculate year if 
 
@@ -45,22 +45,22 @@ OR
  'year' modulus 4 equals to 0 and 'year' modulus 100 equals to 0 and 'year' modulus 400 equals to 0
     DISPLAY "Leap Year"
 ELSE 
-    DISPLAY "Not Leap Year"
+    DISPLAY "Gregorian Year"
 END
 */ 
 
 //Soal 3 Laundry Day
-// for(var pakaian = 1;pakaian <= 20;pakaian++){
-//    if(pakaian == 20){
-//        console.log("Nyalakan mesin cuci")
-//    } else{
-//        console.log("Pakaian belum cukup")
-//    }
-// }
+for(var pakaian = 1;pakaian <= 20;pakaian++){
+   if(pakaian == 20){
+       console.log("Nyalakan mesin cuci")
+   } else{
+       console.log("Pakaian belum cukup")
+   }
+}
 /*
-a. Algoritma
+a. Algorithm
 1. Set 'clothes' with 1
-2. Add 'clothes' 1 by 1 until less than 21
+2. Add 'clothes' 1 by 1 until equals to 20
 3. When 'clothes' value equals to 20, turn on the washing machine
 
 b. Pseudocode
@@ -68,7 +68,7 @@ START
 STORE 'clothes' with 1
 WHILE 'clothes' 1  equal or less than 20
     IF 'clothes' equals to 20
-    DISPLAY "turn on"
+    DISPLAY "turn on washing machine"
 ELSE
     DISPLAY "not yet started"
 ADD 'clothes' by 1
@@ -77,4 +77,25 @@ END WHILE
 */
 
 //Soal 4 Periksa Kuku
+var amountStudent = 40;
+for(var student =1; student <= amountStudent; student++){
+    if(student == 'kuku pendek'){
+        console.log("guru memuji si anak")
+    } else {
+        console.log("guru menghkum si anak")
+    }
+}
+/*
+START
+STORE 'amountStudent' with 40
+FOR STORE 'student' with 1
+LOOPING 'student' until LESS THAN OR EQUAL 'amountStudent'
+ADD 'student' 1 by 1
 
+IF student equals to long nails
+DISPLAY 'Teacher punish student'
+ELSE
+DISPLAY 'Teacher praise student'
+END IF
+END
+*/
